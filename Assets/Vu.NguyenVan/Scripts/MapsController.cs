ce6efbc91629a6a0edaa4151c20b2bd87fb724e6
VuNguyenVan8596 <vunguyen0805@gmail.com>
2018-05-16 16:43:01 +0700

    first commit

---
+++ Assets/Vu.NguyenVan/Scripts/MapsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapsController : MonoBehaviour {
+
+	public void LoadGameA()
+    {
+        Debug.Log("Load Game A");
+    }
+
+    public void LoadGameB()
+    {
+        Debug.Log("Load Game B");
+    }
+
+
+    public void LoadGameC()
+    {
+        Debug.Log("Load Game C");
+    }
+}
---
+++ Assets/Vu.NguyenVan/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public void NewGame(GameObject game)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options");
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
