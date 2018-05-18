37e545c9f72575bdb3ce5541a6af6015d4da7186
VuNguyenVan8596 <vunguyen0805@gmail.com>
2018-05-18 15:21:03 +0700

    map

---
+++ Assets/Vu.NguyenVan/Scripts/CameraMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public Transform target;
+    public float moveSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 p1 = target.position + offset;
+        Vector3 speed = Vector3.Lerp(transform.position, p1, moveSpeed);
+        transform.position = speed;
+
+        transform.LookAt(target);
+    }
+
+
+}
